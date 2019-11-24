5295be29772894505561a8037014a73b958ad36f
mbozecki <56350662+mbozecki@users.noreply.github.com>
2019-11-24 19:40:56 +0100

    Update ver1.cpp
    dodana pierwsza osbluga klawiszow
    dodana pierwsza "plansza" i kulka

--- ver1.cpp
+++ ver1.cpp
@@ -2,22 +2,36 @@
 #include <GL/glut.h>
 #include<iostream>
 #include <stdlib.h>
-
+#include "soil.h"
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 // Deklaracje funkcji, ktore beda uzyte do obslugi roznych zdarzen.
 void OnRender();
 void OnReshape(int, int);
 // Punkt wejscia do programu.
+
+void keyPressed(unsigned char key, int x, int y)
+{
+	if (key == 'a')
+	{ 
+		std::cout << "elo";
+
+	}
+}
+
 int main(int argc, char * argv[])
 {
+
 	// Inicjalizacja biblioteki GLUT. Nalezy przekazac parametry
 	// wywolania programu.
 	glutInit(&argc, argv);
 
 	// Ustawienie parametrow okna i kontekstu OpenGL.
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(640, 360);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH); // bufor klatki w formacie RGBA, double-buffered, z buforem glebokosci
 
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(1024, 768);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH); // bufor klatki w formacie RGBA, double-buffered, z buforem glebokosci
+	glutKeyboardFunc(keyPressed);
 	// Utworzenie wlasciwego okna i nadanie mu tytulu.
 	glutCreateWindow("Projekt CPP czesc 1");
 
@@ -39,10 +53,15 @@ int main(int argc, char * argv[])
 
 // Licznik klatek - uzyteczny przy prostym ruchu kamery.
 int frame = 0;
+float posx = 0.00;
+
+int v = 1;//predkosc 
+
 
 // Callback przerysowujacy klatke.
 void OnRender() {
 
+	glutKeyboardFunc(keyPressed);
 	// Wyczysc zawartosc bufora koloru i glebokosci.
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -57,76 +76,38 @@ void OnRender() {
 	glTranslatef(0.0f, 0.0f, -13.0f);
 
 	// Obrot kamery
-	glRotatef(frame, 0.0f, 1.0f, 0.0f);
+	//glRotatef(frame, 0.0f, 1.0f, 0.0f);
+	glRotatef(0, 1.0f, 0.0f, 0.0f);
 
 
 	// Rysowanie obiektow na scenie.
 
 	// Prostopadloscian/brz
-	glColor3f(1.0f, 0.0f, 0.0f);
+	glColor3f(0.1f, 0.0f, 0.1f);
 	glPushMatrix();
 		glTranslatef(0.0f, 0.5f, 0.0f);
-		glScalef(0.5f, 4.0f, 0.5f);
-		glutSolidCube(1.30f);
-	glPopMatrix();
+		glScalef(7.0f, 10.0f, 0.5f);
+		glutSolidCube(1.00f);
+	glPopMatrix();	
 
-	// Prostopadloscian/noga 1
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glPushMatrix();
-		
-		glTranslatef(-1.6f, -4.0f, 0.0f);
-		glRotatef(320, .0f, 0.0f, 1.0f);
-		glScalef(0.5f, 4.0f, 0.5f);
-		glutSolidCube(0.8f);
-	glPopMatrix();
 
-	// Prostopadloscian/noga 2
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glPushMatrix();
-
-	glTranslatef(+1.6f, -4.0f, 0.0f);
-	glRotatef(40, .0f, 0.0f, 1.0f);
-	glScalef(0.5f, 4.0f, 0.5f);
-	glutSolidCube(0.8f);
-	glPopMatrix();
-
-	// Prostopadloscian/reka 1
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glPushMatrix();
-
-	glTranslatef(-2.50f, +2.0f, 0.0f);
-	glRotatef(280, .0f, 0.0f, 1.0f);
-	glScalef(0.5f, 4.0f, 0.5f);
-	glutSolidCube(0.8f);
-	glPopMatrix();
-
-	// Prostopadloscian/reka 2
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glPushMatrix();
-
-	glTranslatef(+2.50f, +2.0f, 0.0f);
-	glRotatef(80, .0f, 0.0f, 1.0f);
-	//glRotatef(frame, 0.0f, 0.0f, 0.0f);
-	glScalef(0.5f, 4.0f, 0.5f);
-	glutSolidCube(0.8f);
-	glPopMatrix();
-
-	// Kula/leb
+	// Kulka
 	glColor3f(0.0f, 1.0f, 0.0f);
 	glPushMatrix();
-		glTranslatef(0.0f, 4.5f, 0.0f);
-		glutSolidSphere(1.0f, 24, 24);
+		glTranslatef(posx, 4.5f, 0.8f);
+		glutSolidSphere(.2f, 24, 24);
 	glPopMatrix();
+		
+	if (posx < 7)
+	{
+		posx += 0.01;
+		
 
-	// Stozek/czapka
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glPushMatrix();
-		glTranslatef(0.0f, 6.0f, 0.0f);
-		glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-		glutSolidCone(.5f, 1.0f, 14, 14);
-	glPopMatrix();
-	
-
+	}
+	else
+	{
+		posx = posx - 0.1;
+	}
 	// Jesli instrukcje w danej implementacji OpenGL byly buforowane,
 	// w tym momencie bufor zostanie oprozniony a instrukcje wykonane.
 	glFlush();
